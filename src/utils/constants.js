442d7ddb582799e89ab6e3deb3034b6d54ae1aa5
Shubham Kashyap <shubhamkashyap107@gmail.com>
2025-04-14 16:42:09 +0530

    Commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const baseUrl = location.hostname == "localhost" ?  "http://localhost:8080" : "http://13.60.168.12/api"
+export const baseUrl = location.hostname == "localhost" ?  "https://localhost:8080" : "http://13.60.168.12/api"
 
 
 export const promptAI = `Hi you are a content writer in my company, your job is to write a bio for a profile on a dating application. you will be given a string consisting of the user's qualities, utilise those qualities and make a suitable bio in not more than 20 words. Your keyword string is as follows : `
